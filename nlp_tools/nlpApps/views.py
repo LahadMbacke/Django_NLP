a0839edfde5846cccca074ba40726676b993f132
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-03-27 00:22:22 +0100

    ajout des fonctions et entrer txt pour NER

--- nlp_tools/nlpApps/views.py
+++ nlp_tools/nlpApps/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import spacy
+from spacy import displacy
+nlp = spacy.load("fr_core_news_md")
 
 def home(request):
     return render(request, 'nlpApps/ner.html')
+
+
+def submit_form_ner(request):
+    if request.method == 'POST':
+        text = request.POST['user_input']
+        doc = nlp(text)
+        html = displacy.render(doc, style="ent")
+        return HttpResponse(html)
+    else:
+        return HttpResponse('Invalid request')
