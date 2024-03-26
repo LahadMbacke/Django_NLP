bc07625cdb1c23e227f0e7eb7fdfeb5c088e4bdb
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-03-26 23:58:36 +0100

    Ajout templete et static

--- nlp_tools/nlp_tools/urls.py
+++ nlp_tools/nlp_tools/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from nlpApps import views as view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', view.home)
 ]
